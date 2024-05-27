d64c2b0f64ca49a38933c1b5a00c028b496fd6b2
Stephen <stewxy13@gmail.com>
2024-05-27 22:29:18 +1200

    Implemented calls to bazaar API to get item listings

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ from requests.auth import HTTPBasicAuth
 api_key = ""
 api_uuid = ""
 
-url = "https://api.hypixel.net/v2/skyblock/profile"
 api_url = "https://api.hypixel.net/skyblock/profiles?key={key}&uuid={uuid}".format(key = api_key, uuid = api_uuid)
 response = requests.get(api_url)
 
 profiles = (response.json()).get("profiles")
+#print(profiles)
+# collection = sorted(profiles[0].get('members').get(api_uuid).get("unlocked_coll_tiers"))
+# print(collection)
 collection = sorted(profiles[0].get('members').get(api_uuid).get("unlocked_coll_tiers"))
-print(collection)
+#print(collection)
+
+u2 = "https://api.hypixel.net/v2/skyblock/bazaar"
+r = requests.get(u2)
+x = r.json().get("products").get("ESSENCE_DRAGON").get("quick_status")
+print(x)
+# for item in x:
+#     print(item)
+
 
 
