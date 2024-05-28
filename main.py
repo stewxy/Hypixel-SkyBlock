266d80fa3feaaabff94cc4c48f93da0e6048f22a
Stephen <stewxy13@gmail.com>
2024-05-28 15:50:54 +1200

    Implemented Discord Bot functionality

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import requests
+import discord
+from discord.ext import commands
 from requests.auth import HTTPBasicAuth
+
+intents = discord.Intents.all()
+client = commands.Bot(command_prefix="!", intents=intents)
 
 api_key = ""
 api_uuid = ""
+
+
+@client.event
+async def on_ready():
+    print("Online")
+
+
+@client.command(aliases=['p'])
+async def ping(ctx):
+    await ctx.send("pong")
+
 
 api_url = "https://api.hypixel.net/skyblock/profiles?key={key}&uuid={uuid}".format(key = api_key, uuid = api_uuid)
 response = requests.get(api_url)
@@ -16,10 +32,11 @@ collection = sorted(profiles[0].get('members').get(api_uuid).get("unlocked_coll_
 
 u2 = "https://api.hypixel.net/v2/skyblock/bazaar"
 r = requests.get(u2)
-x = r.json().get("products").get("ESSENCE_DRAGON").get("quick_status")
+x = r.json().get("products").get("ENDER_PEARL").get("quick_status")
 print(x)
 # for item in x:
 #     print(item)
 
 
 
+client.run("")
